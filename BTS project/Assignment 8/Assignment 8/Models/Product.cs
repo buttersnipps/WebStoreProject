27e00768922540542045766713fb89b3aaf0e6d9
MikeVuong <michael.vuong@live.ca>
2017-03-18 12:43:13 -0400

    Backup Created

---
+++ BTS project/Assignment 8/Assignment 8/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_8.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductImage { get; set; }
+        public int Quantity { get; set; }
+        public int ReloadValue { get; set; }
+        public double ProductWeight { get; set; }
+        public double ProductLength { get; set; }
+        public double ProductWidth { get; set; }
+        public double ProductHeight { get; set; }
+
+        //Associations
+        public int PromotionId { get; set; }
+        public Promotion Promotion { get; set; }
+        public decimal PromoPrice { get; set; }
+        public virtual ICollection<CategoryToProducts> CategoryToProducts { get; set; }
+        public Manufacture Manufacture { get; set; }
+        public Condition Condition { get; set; }
+        public string OrderName { get; set; }
+        public virtual List<OrderDetail> OrderDetails { get; set; }
+       
+    }
+}
